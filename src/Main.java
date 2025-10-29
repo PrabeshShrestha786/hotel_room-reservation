a2d64a69ced284b1d120f5b019ea6241b642ed73
Prabesh786 <e2301430@vamk.fi>
2025-10-29 10:30:09 +0200

    change

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,5 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello from Docker + VS Code collaboration!");
-        System.out.println("this is added by mr farhan");
     }
 }
